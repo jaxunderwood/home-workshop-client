38b75ce8e31c7e4f198001a1dac232053e5b412e
jaxunderwood <jax.hudson@gmail.com>
2026-04-27 18:11:16 -0700

    updated home component

--- workshop-client/src/Components/Home.jsx
+++ workshop-client/src/Components/Home.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/App.module.css';
 
 
 
 export default function Home() {
+const [weeklyTip, setWeeklyTip] = useState(null);
+
+    useEffect(() => {
+        // Fetch the specific weeklyTip object
+        fetch('http://localhost:3001/weeklyTip')
+            .then(res => res.json())
+            .then(data => setWeeklyTip(data))
+            .catch(err => console.error("Error fetching tip:", err));
+    }, []);
+
+
+
     return (
         <div>
  <section className={styles.hero}>
@@ -45,14 +58,24 @@ export default function Home() {
             </div>
         </section>
         <aside className={styles.aside}>
+            {/* {weeklyTip ? (
+                <> */}
             <img className={styles.imagefw} src="images/ceiling-fan.jpg"
-                alt="still ceiling fan in living room with window in the background" />
+            // {weeklyTip.image}
+                alt="still ceiling fan in living room with window in the background"
+                //{weeklyTip.imageAlt}
+                />
             <div className={styles.tipBox}>
                 <h1>Tip of the week</h1>
+                {/* {weeklyTip.title} */}
                 <p>Ceiling fans should run <strong>counterclockwise</strong> in the summer at higher speeds to create a
                     cool, downdraft breeze. In the winter, set them to run <strong>clockwise</strong> at a low speed to
                     create an updraft that pulls cool air up and pushes warm air down from the ceiling</p>
+                    {/* {weeklyTip.text} */}
             </div>
+            {/* </>
+            ) : ( <p>Loading this week's home tip . . .</p>
+            )} */}
         </aside>
         </div>
     );
